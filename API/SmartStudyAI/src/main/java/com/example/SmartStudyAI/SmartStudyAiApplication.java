60dd00dcf39a4f6eb72943da561ce3f5ee9cc712
rafaelmaghinay <rafaelfmaghinay@su.edu.ph>
2025-09-08 12:50:37 +0800

    first commit

---
+++ API/SmartStudyAI/src/main/java/com/example/SmartStudyAI/SmartStudyAiApplication.java
@@ -0,0 +1,13 @@
+package com.example.SmartStudyAI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartStudyAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartStudyAiApplication.class, args);
+	}
+
+}
