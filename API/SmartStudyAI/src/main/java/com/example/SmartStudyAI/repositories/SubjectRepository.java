ef89c4178e9b5a189395c07bbf27cf545708d469
rafaelmaghinay <rafaelfmaghinay@su.edu.ph>
2025-10-03 21:07:10 +0800

    retrieve questions, submit answers, new links

--- API/SmartStudyAI/src/main/java/com/example/SmartStudyAI/repositories/SubjectRepository.java
+++ API/SmartStudyAI/src/main/java/com/example/SmartStudyAI/repositories/SubjectRepository.java
@@ -3,8 +3,10 @@ package com.example.SmartStudyAI.repositories;
 import com.example.SmartStudyAI.model.Subject;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface SubjectRepository extends JpaRepository<Subject, Long> {
     Subject findByName(String name);
+    List<Subject> findAllByUserId(Long userId);
 }
