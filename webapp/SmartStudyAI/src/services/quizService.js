7a71dae4e3776f15b0d1e561cf5de504aa13993f
FoxtailRamen <145954258+FoxtailRamen@users.noreply.github.com>
2025-10-23 16:25:59 +0800

    Quiz functionality Attempt
    Added quiz service and quiz functionality. Unable to test due to still non-working API

--- webapp/SmartStudyAI/src/services/quizService.js
+++ webapp/SmartStudyAI/src/services/quizService.js
@@ -2,15 +2,28 @@ import { apiRequest } from "./apiRequest.js";
 
 export const quizService = {
   generate: async (subjectId, notesId) => {
-    return apiRequest("/quizzes/generate", {
+    return apiRequest("/quizzes/generation", {
       method: "POST",
-      body: JSON.stringify({ email, password }),
+      body: JSON.stringify({ subjectId, notesId }),
     });
   },
 
-  getQuiz: async (name, email, password) => {
+  getUserQuizzes: async () => {
     return apiRequest("/quizzes/user/me", {
       method: "GET",
+    });
+  },
+
+  getQuizQuestions: async (quizId) => {
+    return apiRequest(`/quizzes/questions/${quizId}`, {
+      method: "GET",
+    });
+  },
+
+  submitQuizAnswers: async (quizId, answers) => {
+    return apiRequest(`/quizzes/user/me/quiz/${quizId}/submit`, {
+      method: "POST",
+      body: JSON.stringify(answers),
     });
   },
 };
